40d92b612d67404e8ca0aa8571b78f4674cb11b5
Mudit Agarwal <mstar@Mudits-MacBook-Pro.local>
2022-07-07 13:30:37 +0530

    fix: start logic

--- Week-4/engine.py
+++ Week-4/engine.py
@@ -6,8 +6,8 @@ class Game:
     def __init__(self) -> None:
         self.__word_generator = WordGenerator()
         self.__hangman = HangMan(self.__word_generator.get_random_word())
-        self.first_turn = 1
         self.is_active = 1
+        print(self.__hangman.render())
 
     @staticmethod
     def print_rules() -> None:
@@ -19,21 +19,20 @@ class Game:
 
     def play(self) -> None:
         while 1:
-            if self.first_turn:
-                print(self.__hangman.render())
             if self.is_active:
                 line = 'Please enter a letter to guess '
             else:
                 line = 'Please start a new game '
+
             player_input = input(line).upper()
+
             if player_input == "START":
                 self.__hangman = HangMan(self.__word_generator.get_random_word())
-                self.first_turn = 1
                 self.is_active = 1
+                print(self.__hangman.render())
             elif player_input == "QUIT":
                 break
             elif self.is_active:
-                self.first_turn = 0
                 if self.__hangman.move(player_input[0]):
                     self.is_active = 0
 
