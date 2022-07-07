33770db7e6af37f86bb920150c60eb645c568def
Mudit Agarwal <mstar@Mudits-MacBook-Pro.local>
2022-07-07 17:04:40 +0530

    version:  final version of HangMan Game(without hint)

--- Week-4/engine.py
+++ Week-4/engine.py
@@ -5,41 +5,47 @@ from HangMan import HangMan
 class Game:
     def __init__(self) -> None:
         self.__word_generator = WordGenerator()
-        # self.__hangman = HangMan("FOOD")
-        self.__hangman = HangMan(self.__word_generator.get_random_word())
-        self.is_active = 1
+        self.__hangman=None
+        self.is_active = 0
+        game.print_rules()
+        self.start_a_new_game()
 
-    def print_rules(self) -> None:
+    @staticmethod
+    def print_rules() -> None:
         print("!!!HANGMAN!!!\n")
         print("*******RULES*******")
         print("Type Start for starting a new game: ")
         print("Type Quit for quiting a new game: ")
         print("**************\n")
+
+    def start_a_new_game(self):
+        self.__hangman = HangMan(self.__word_generator.get_random_word())
+        self.is_active = 1
         print(self.__hangman.render())
 
-    def play(self) -> None:
+    def play(self, entered_letter):
+        self.is_active = self.__hangman.move(entered_letter)
+        if self.is_active:
+            print(self.__hangman.render())
+
+    def player_input(self) -> None:
         while 1:
             if self.is_active:
                 line = 'Please enter a letter to guess '
             else:
                 line = 'Please start a new game '
-
             player_input = input(line).upper()
+
             if len(player_input) == 0:
                 continue
             elif player_input == "START":
-                self.__hangman = HangMan(self.__word_generator.get_random_word())
-                self.is_active = 1
-                print(self.__hangman.render())
+                self.start_a_new_game()
             elif player_input == "QUIT":
                 break
             elif self.is_active:
-                self.is_active = self.__hangman.move(player_input[0])
-                if self.is_active:
-                    print(self.__hangman.render())
+                self.play(player_input[0])
 
 
 if __name__ == '__main__':
     game = Game()
-    game.print_rules()
-    game.play()
+    game.player_input()
--- Week-4/test_HangMan.py
+++ Week-4/test_HangMan.py
@@ -3,9 +3,10 @@ from HangMan import HangMan
 
 
 class TestHangMam(unittest.TestCase):
-    def test_render(self):
+    def test_render(self) -> None:
         hangman = HangMan("FOOD")
-        self.assertEqual(hangman.render(), "_ _ _ _")
+        hangman.payer_guess_letter("O")
+        self.assertEqual(hangman.render(), "_ O O _")
         hangman = HangMan("MUDIT")
         self.assertEqual(hangman.render(), "_ _ _ _ _")
         hangman = HangMan("H")
@@ -13,7 +14,7 @@ class TestHangMam(unittest.TestCase):
         hangman = HangMan("H23")
         self.assertEqual(hangman.render(), "_ _ _")
 
-    def test_payer_guess_letter(self):
+    def test_payer_guess_letter(self) -> None:
         hangman = HangMan("FOOD")
         self.assertEqual(hangman.payer_guess_letter("F"), "Wow! You have discovered 1 new letter")
         self.assertEqual(hangman.payer_guess_letter("F"), "Ohh! You have already guessed the letter")
@@ -26,7 +27,7 @@ class TestHangMam(unittest.TestCase):
         self.assertEqual(hangman.payer_guess_letter("W"), "Sorry! Wrong guess")
         self.assertEqual(hangman.payer_guess_letter("P"), "Game Over\nThe word is MAN")
 
-    def test_move(self):
+    def test_move(self) -> None:
         hangman = HangMan("FOOD")
         hangman.move('f')
         self.assertEqual(hangman.move('o'), 1)
